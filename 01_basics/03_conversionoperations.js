5d1bcb4cf97270edf97f86cfddaf5f79e3cd5584
AMAN RATHOD <124860595+amanrathod07@users.noreply.github.com>
2024-02-07 10:53:25 +0000

    conversionofdatatypes

---
+++ 01_basics/03_conversionoperations.js
@@ -0,0 +1,25 @@
+let score =33
+
+//console.log(typeof score);
+
+let abc =null;
+
+//console.log(typeof abc);
+
+//console.log(abc);
+
+let isloggedin=""
+
+let booleanisloggedin = Boolean(isloggedin)
+
+console.log(booleanisloggedin);
+
+let ads=33
+
+let udit = String(ads)
+
+console.log(udit)
+
+console.log(typeof udit)
+
+
