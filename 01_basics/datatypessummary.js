9ec907b1c23ce536818875592145b911ffc718d2
AMAN RATHOD <124860595+amanrathod07@users.noreply.github.com>
2024-02-09 08:12:22 +0000

    datatypessummary

---
+++ 01_basics/datatypessummary.js
@@ -0,0 +1,17 @@
+// primitive
+
+// number,string,boolean,null,undefined,symbol,bigint
+
+const score=100
+
+const scorevalue=100.3
+
+const isloggedin = false
+const outsidetemp =null
+
+const id=Symbol('123')
+
+const anotherid= Symbol('123')
+
+console.log(id===anotherid)
+
